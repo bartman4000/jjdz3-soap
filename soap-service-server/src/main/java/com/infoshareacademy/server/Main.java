a3a10e88f51be4c0cea2b06d15d8a478f1b390e6
Michal Nowakowski <michal@nowakowski.me.uk>
2017-07-14 22:32:22 +0200

    Initial commit

---
+++ soap-service-server/src/main/java/com/infoshareacademy/server/Main.java
@@ -0,0 +1,12 @@
+package com.infoshareacademy.server;
+
+import com.infoshareacademy.service.AcademyServiceImpl;
+import javax.xml.ws.Endpoint;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Server started...");
+    }
+}
